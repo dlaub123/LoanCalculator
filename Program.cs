f25e948e2742be9bff5d740fe15c6bf55e7d85af
David Laub <dmlaub123@gmail.com>
2020-12-02 15:30:05 -0600

    First pass: the basic monthly calcualtion formula No refactoring to subroutine No formatting of result to two decimal places No amortization schedule

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace LoanCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Loan Calculator");
+            var loanAmount = 74000 * 0.8;
+            var interest = 9.75;
+            var numberOfYears = 29;
+
+            // rate of interest and number of payments for monthly payments
+            var rateOfInterest = interest / (12 * 100);
+            var numberOfPayments = numberOfYears * 12;
+
+            // loan amount = (interest rate * loan amount) / (1 - (1 + interest rate)^(number of payments * -1))
+            var paymentAmount = (rateOfInterest * loanAmount) / (1 - Math.Pow(1 + rateOfInterest, numberOfPayments * -1));
+
+            Console.WriteLine(paymentAmount);
         }
     }
 }

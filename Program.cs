0480c7c9fb44db0d9a48f72c47fac41ffa83b521
David Laub <dmlaub123@gmail.com>
2020-12-03 13:03:23 -0600

    further refactor to make more internal calcs internal to CalcLoanamt function

--- Program.cs
+++ Program.cs
@@ -7,20 +7,24 @@ namespace LoanCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Loan Calculator");
-            var loanAmount = 74000 * 0.8;
+
+            var downPayment = 20; // in Percent
+            var loanAmount = 74000;
             var interest = 9.75;
             var numberOfYears = 29;
 
-            // rate of interest and number of payments for monthly payments
-            var rateOfInterest = interest / (12 * 100);
-            var numberOfPayments = numberOfYears * 12;
-
-            double paymentAmount = CalcLoanAmt(loanAmount, rateOfInterest, numberOfPayments);
+            double paymentAmount = CalcLoanAmt(loanAmount, interest, numberOfYears, downPayment);
 
             Console.WriteLine(paymentAmount); // 2 decimal places!
 
-            static double CalcLoanAmt(double loanAmount, double rateOfInterest, int numberOfPayments)
+            static double CalcLoanAmt(double loanAmount, double interest, int numberOfYears, int downPayment) // make downPayment an optional arg w/default value of 0
             {
+                // subtract down payment from load amount
+                var downPaymentAmount = (100 - downPayment) / 100.0; // implicit cast to float - otherwise will truncate value to 0!
+                loanAmount *= downPaymentAmount;
+                // rate of interest and number of payments for monthly payments
+                var rateOfInterest = interest / (12 * 100); // add to method!
+                var numberOfPayments = numberOfYears * 12;  // add to method!
                 // loan amount = (interest rate * loan amount) / (1 - (1 + interest rate)^(number of payments * -1))
                 return (rateOfInterest * loanAmount) / (1 - Math.Pow(1 + rateOfInterest, numberOfPayments * -1));
             }
